10261088b1e4f5d1c72b5132cc4ff7e0d9bd3e34
sledziu32 <sledziu32@users.noreply.github.com>
2017-07-20 09:54:48 +0200

    Add files via upload

---
+++ bloki/funk.py
@@ -0,0 +1,9 @@
+def glebogryzarka():
+    t = 0
+    for t in range(10):
+        print("X"*t)
+        t =+ 1
+
+def clone(s):
+        print(s*2)
+
---
+++ nauka.py
@@ -0,0 +1,6 @@
+from bloki.funk import glebogryzarka #po funk NIE MA .PY !!!
+from bloki.funk import clone
+print("\ncoś")
+#glebogryzarka()
+s = input("podaj tekst do sklonowania: ")
+clone(s) #???
